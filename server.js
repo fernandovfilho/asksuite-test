1613ae5233c3e905042e185ab2677fa286b998cf
Fernando Vargas <fernandov.filho@gmail.com>
2021-08-06 02:54:31 -0300

    docs: Added documentation for search route
    Added the Swagger UI to show POST /search documentation

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 require("dotenv").config();
 const express = require("express");
 const router = require("./routes/router.js");
+const cors = require("cors");
+const swaggerUi = require("swagger-ui-express");
+const swaggerDocument = require("./swagger.json");
 
 const app = express();
 app.use(express.json());
+app.use(cors());
+
+// Documentation as a plus
+app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 const port = process.env.PORT;
 
